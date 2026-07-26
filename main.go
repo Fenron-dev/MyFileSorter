fd0d091a07c71c02ea72be6c6787636042090707
Fenron-dev <56755865+Fenron-dev@users.noreply.github.com>
2026-07-26 13:09:14 +0200

    style: apply canonical drop option formatting

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ func main() {
 		},
 		BackgroundColour: &options.RGBA{R: 244, G: 241, B: 234, A: 255},
 		OnStartup:        app.startup,
-		DragAndDrop:     &options.DragAndDrop{
+		DragAndDrop: &options.DragAndDrop{
 			EnableFileDrop: true,
 		},
 		Bind: []interface{}{
